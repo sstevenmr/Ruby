c5b14116c2cb313122ff2bc09ca5c6ad14eb39f7
Steven <nachoscc7@gmail.com>
2014-04-30 17:02:47 -0600

    Laboratorio

---
+++ Laboratorios/Laboratorio/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestLab2::Application.config.secret_key_base = 'fe05789622b88c3a98fbef370f72a405b66625b5fc2ceb4ef2baa3b482179e34eebf34569f460f2749184e6df5c2074a1b05ff6c7922ed79ac0dfa2a1900e4dc'
